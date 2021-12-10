2bb1a0b17f7ee223ab355e7517a27a27852aa08d
emmacneil <emac021@gmail.com>
2021-12-09 23:16:11 -0400

    Improve solution to day 3, part 1

--- day3-binary/part1.cpp
+++ day3-binary/part1.cpp
@@ -1,34 +1,43 @@
+#include <chrono>
 #include <iostream>
 #include <stdint.h>
 #include <string>
+#include <vector>
 
 int main(int argc, char *argv[])
 {
+	std::chrono::system_clock::time_point t0 = std::chrono::system_clock::now();
+
 	std::string bit_string;
+	int n_numbers = 0;
 
 	// Read first line to determine number of bits per line
 	std::cin >> bit_string;
-	int n_bits = bit_string.size();
-	int ones[n_bits] = {0};
-	
-	// Count the number of 1's in each position across all lines
-	int n_lines = 0;
+	std::vector<int> common(bit_string.size(), 0); // array to count number of times 1 appears in each bit position
 	do
 	{
-		for (int i = 0; i < n_bits; i++)
-			ones[i] += bit_string.at(i) == '1' ? 1 : 0;
-		n_lines++;
+		n_numbers++;
+		for (int i = 0; i < bit_string.size(); i++)
+			if (bit_string.at(i) - '0')
+				common[i]++;
 	} while (std::cin >> bit_string);
 
-	// Construct gamma and epsilon.
-	// Epsilon is calculated as the bitwise NOT of gamma.
-	uint64_t gamma = 0;
-	for (int i = 0; i < n_bits; i++)
-		gamma = (gamma << 1) ^ (ones[i] > n_lines / 2 ? 1 : 0);
-	uint64_t mask = (1 << n_bits) - 1;
-	uint64_t epsilon = ~gamma & mask;
-	
-	std::cout << gamma*epsilon << std::endl;
+	uint64_t gamma = 0, epsilon = 0;
+	for (int n : common)
+	{
+		gamma <<= 1;
+		epsilon <<= 1;
+		if (n > n_numbers / 2)
+			gamma |= 1;
+		else
+			epsilon |= 1;
+	}
+
+	std::cout << epsilon*gamma << std::endl;
+
+	std::chrono::system_clock::time_point t1 = std::chrono::system_clock::now();
+    std::chrono::duration<double> dt = t1 - t0;
+    std::cout << "Elapsed time: " << dt.count() << " seconds." << std::endl;
 
 	return 0;
 }
