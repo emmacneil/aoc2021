b70c21dde54662d93b3ac7c9c50cd75119d58406
emmacneil <emac021@gmail.com>
2021-12-13 16:43:43 -0400

    Optimize day 13, part 2

--- 13-origami/part2.cpp
+++ 13-origami/part2.cpp
@@ -5,15 +5,13 @@
 #include <utility>
 #include <vector>
 
-struct paper
+int shrink(int x, int w)
 {
-    paper(const std::vector<std::pair<int, int>>& dots, int x_max, int y_max);
-    paper(const paper& other, char dir, int z);
-    ~paper();
-    void print();
-    int w, h;
-    bool** data;
-};
+    w = w + 1;
+    int ret = x % w;
+    if ((x / w) & 1) ret = w - ret - 2;
+    return ret;
+}
 
 int main(int argc, char *argv[])
 {
@@ -22,7 +20,7 @@ int main(int argc, char *argv[])
     int x_max = 0, y_max = 0;
     std::vector<std::pair<int, int>> coords;
 
-    // Get input
+    // Get dot coordinates
     std::string line;
     while (getline(std::cin, line))
     {
@@ -31,87 +29,40 @@ int main(int argc, char *argv[])
         std::string x_str, y_str;
         getline(ss, x_str, ',');
         getline(ss, y_str);
-        int x = atoi(x_str.c_str());
-        int y = atoi(y_str.c_str());
-        x_max = x > x_max ? x : x_max;
-        y_max = y > y_max ? y : y_max;
-        coords.push_back(std::make_pair(x, y));
+        coords.push_back(std::make_pair(atoi(x_str.c_str()), atoi(y_str.c_str())));
     }
 
-    paper *p = new paper(coords, x_max, y_max);
+    int w = 0, h = 0;
     while (getline(std::cin, line))
     {
-        std::stringstream ss(line);
-        std::string str;
-        getline(ss, str, '=');
-        char dir = str.back();
-        getline(ss, str);
-        int z = atoi(str.c_str());
-        
-        paper* q = p;
-        p = new paper(*p, dir, z);
-        delete q;
+        char ch = line[11];
+        if (ch == 'x')
+            w = atoi(line.c_str() + 13);
+        else
+            h = atoi(line.c_str() + 13);
+    }
+    bool** out = new bool*[w];
+    for (int x = 0; x < w; x++)
+        out[x] = new bool[h] {false};
+
+    for (const auto& pr : coords)
+        out[shrink(pr.first, w)][shrink(pr.second, h)] = true;
+
+    for (int y = 0; y < h; y++)
+    {
+        for (int x = 0; x < w; x++)
+            std::cout << (out[x][y] ? '*' : ' ');
+        std::cout << std::endl;
     }
 
-    p->print();
+    for (int x = 0; x < w; x++)
+        delete[] out[x];
+    delete[] out;
 
     std::chrono::system_clock::time_point t1 = std::chrono::system_clock::now();
     std::chrono::duration<double> dt = t1 - t0;
     std::cout << "Elapsed time: " << dt.count() << " seconds." << std::endl;
+    // Seems like roughly half the elapsed time is cleanup and printing.
 
     return 0;
-}
-
-paper::paper(const std::vector<std::pair<int, int>>& dots, int x_max, int y_max)
-    : w(x_max + 1), h(y_max + 1)
-{
-    data = new bool* [w];
-    for (int i = 0; i < w; i++)
-        data[i] = new bool[h] {false};
-    for (const auto& dot : dots)
-        data[dot.first][dot.second] = true;
-}
-
-paper::~paper()
-{
-    for (int i = 0; i < w; i++)
-        delete[] data[i];
-    delete[] data;
-}
-
-paper::paper(const paper& other, char dir, int z)
-    : w(other.w), h(other.h)
-{
-    if (dir == 'x')
-    {
-        w = z;
-        data = new bool* [w];
-        for (int x = 0; x < w; x++)
-            data[x] = new bool[h] {false};
-        for (int x = 0; x < w; x++)
-            for (int y = 0; y < h; y++)
-                data[z - x - 1][y] = other.data[z - x - 1][y] | other.data[z + x + 1][y];
-    }
-    else
-    {
-        h = z;
-        data = new bool* [w];
-        for (int x = 0; x < w; x++)
-            data[x] = new bool[h] {false};
-        for (int x = 0; x < w; x++)
-            for (int y = 0; y < h; y++)
-                data[x][z - y - 1] = other.data[x][z - y - 1] | other.data[x][z + y + 1];
-    }
-}
-
-void paper::print()
-{
-    for (int y = 0; y < h; y++)
-    {
-        for (int x = 0; x < w; x++)
-        {
-            std::cout << (data[x][y] ? '*' : ' ');
-        }
-        std::cout << std::endl;
-    }       
 }
